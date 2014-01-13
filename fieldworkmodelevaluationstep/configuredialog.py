16726844c4e4671fc72f3c2391b7f7af2556753f
Ju Zhang <ju@tomahawk.(none)>
2014-01-13 16:07:52 +1300

    fixed typo in configuredialogue import

--- fieldworkmodelevaluationstep/configuredialog.py
+++ fieldworkmodelevaluationstep/configuredialog.py
@@ -1,7 +1,7 @@
 
 
 from PySide import QtGui
-from fieldworkmodelevaluationstep.ui_configuredialog import Ui_ConfigureDialog
+from fieldworkmodelevaluationstep.ui_configuredialog import Ui_Dialog
 
 INVALID_STYLE_SHEET = 'background-color: rgba(239, 0, 0, 50)'
 DEFAULT_STYLE_SHEET = ''
