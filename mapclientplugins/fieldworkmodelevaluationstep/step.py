f02e7225151d4b8708515d72274d6bda71c4f873
TimmyNic <tim.nicolas@outlook.com>
2021-06-08 10:26:56 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/fieldworkmodelevaluationstep/step.py
+++ mapclientplugins/fieldworkmodelevaluationstep/step.py
@@ -109,7 +109,7 @@ class FieldworkModelEvaluationStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog(QtWidgets.QApplication.activeWindow().currentWidget())
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
